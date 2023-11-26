0afb5f7360b888dd19812b98c56a92318ca2665d
Viv231912 <111347784+Viv231912@users.noreply.github.com>
2023-11-26 15:28:46 +1100

    final with deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,138 +1,189 @@
-import { useMemo } from "react";
-import { Link } from "react-router-dom"
-import { useAuthContext  } from "../context/AuthContext"
+import { useMemo, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+import { useFirestoreContext } from "../context/FirestoreContext";
 
 const LogIn = () => {
-  const {login, currentUser } = useAuthContext() 
+  const { login, currentUser } = useAuthContext();
   return (
-   !currentUser && 
-   <button type="button" className="btn btn-warning" onClick={login}>
-      Login
-    </button>
+    !currentUser && (
+      <button type="button" className="btn btn-warning" onClick={login}>
+        Login
+      </button>
+    )
   );
 };
 
 const LogOut = () => {
-  const { logout, currentUser } = useAuthContext()
-  return(
-   !!currentUser && (
-    <button type="button" className="btn btn-danger" onClick={logout}>
-       Logout
-     </button>
-     )
+  const { logout, currentUser } = useAuthContext();
+  return (
+    !!currentUser && (
+      <button type="button" className="btn btn-danger" onClick={logout}>
+        Logout
+      </button>
+    )
   );
- }
+};
 
 function Navigation() {
-  const { currentUser } = useAuthContext() 
-  return(
-   
+  const { currentUser } = useAuthContext();
+  const { pathname } = useLocation();
+  return (
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-    {/* remove all links except HOME */}
-    <li className="nav-item">
-      <Link className="nav-link active" aria-current="page" to="/">
-        Home
-      </Link>
-    </li>
-    <li className="nav-item">
-      {currentUser && 
-      <Link className="nav-link active" aria-current="page" to="/StockImages">
-        My Stock Images
-      </Link>}
-    </li>
-  </ul>
-  )
+      {/* remove all links except HOME */}
+      <li className="nav-item">
+        <Link
+          className={`nav-link ${pathname === "/" ? "active" : ""}`}
+          aria-current="page"
+          to="/"
+        >
+          Home
+        </Link>
+      </li>
+      {currentUser && (
+        <li className="nav-item">
+          {" "}
+          <Link
+            className={`nav-link ${
+              pathname === "/StockImages" ? "active" : ""
+            }`}
+            aria-current="page"
+            to="/StockImages"
+          >
+            My Stock Images
+          </Link>
+        </li>
+      )}
+      {currentUser && (
+        <li className="nav-item">
+          {" "}
+          <Link
+            className={`nav-link ${pathname === "/profile" ? "active" : ""}`}
+            aria-current="page"
+            to="/profile"
+          >
+            Profile
+          </Link>
+        </li>
+      )}
+    </ul>
+  );
 }
 
 function SearchForm() {
-  return(
-    <form className="d-flex">
-    <input
-      className="form-control me-2"
-      type="search"
-      placeholder="Search"
-      aria-label="Search"
-    />
-    <button className="btn btn-outline-success" type="submit">
-      Search
-    </button>
-  </form>
-  )
+  const [text, search] = useState(null);
+  const { filterItems: filter } = useFirestoreContext();
+  const handleOnChange = (e) => {
+    search(e.target.value);
+    filter(e.target.value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault(); //prevent default behaviour, when we submit the page doesn't refresh
+    filter(text); // make sure it's working
+  };
+
+  return (
+    <form className="d-flex" onSubmit={handleOnSubmit}>
+      <input
+        onChange={handleOnChange}
+        className="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <button className="btn btn-outline-success" type="submit">
+        Search
+      </button>
+    </form>
+  );
 }
 
 function Dropdown() {
-  const { currentUser } = useAuthContext()
- 
-  const username = useMemo(()=> {
-    return currentUser?.displayName || "Profile"
-  }, [currentUser])
+  const { currentUser } = useAuthContext();
 
-  const avatar = useMemo(()=> {
-return !!currentUser ? 
-<img className="avatar" src={currentUser?.photoURL} alt={currentUser?.displayName} width="34" height="34"/> : "Login"
-  }, [currentUser])
-  return( 
-  <ul className="navbar-nav mb-2 mb-lg-0" >
-  {" "}
-  {/* remove ms-auto */}
-  <li className="nav-item dropdown">
-    <a
-      className="nav-link dropdown-toggle"
-      href="#"
-      id="navbarDropdown"
-      role="button"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      {avatar}
-    </a>
-    <ul className="dropdown-menu" aria-labelledby="navbarDropdown" style={{right:0, left: 'auto'}}>
-      <li>
-        <a className="dropdown-item text-center" href="#">
-          {username}
+  const username = useMemo(() => {
+    return currentUser?.displayName || "Profile";
+  }, [currentUser]);
+
+  const avatar = useMemo(() => {
+    return !!currentUser ? (
+      <img
+        className="avatar"
+        src={currentUser?.photoURL}
+        alt={currentUser?.displayName}
+        width="34"
+        height="34"
+      />
+    ) : (
+      "Login"
+    );
+  }, [currentUser]);
+  return (
+    <ul className="navbar-nav mb-2 mb-lg-0">
+      {" "}
+      {/* remove ms-auto */}
+      <li className="nav-item dropdown">
+        <a
+          className="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdown"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {avatar}
         </a>
-      </li>
-      <li><hr className="dropdown divider"/></li> 
-      <li>
-      <div className="d-flex justify-content-center">
-        <LogIn />
-        <LogOut />
-      </div>
+        <ul
+          className="dropdown-menu"
+          aria-labelledby="navbarDropdown"
+          style={{ right: 0, left: "auto" }}
+        >
+          <li>
+            <a className="dropdown-item text-center" href="#">
+              {currentUser && <Link to="/profile">{username}</Link>}
+            </a>
+          </li>
+          <li>
+            <hr className="dropdown divider" />
+          </li>
+          <li>
+            <div className="d-flex justify-content-center">
+              <LogIn />
+              <LogOut />
+            </div>
+          </li>
+        </ul>
       </li>
     </ul>
-  </li>
-</ul>
-
-)
+  );
 }
 
-
 function Navbar() {
-    return(
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        ⚡ Firestock
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#nav"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <Navigation />
-        <SearchForm />
-        <Dropdown />
-  
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          ⚡ Firestock
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#nav"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <Navigation />
+          <SearchForm />
+          <Dropdown />
+        </div>
       </div>
-    </div>
     </nav>
-    )
-  }
-  export default Navbar
+  );
+}
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 import Layout from "./components/Layout";
-import reportWebVitals from './reportWebVitals';
-import Provider from '../src/context/FirestoreContext';
-import AuthProvider, { useAuthContext } from './context/AuthContext';
-import StockImages from './components/StockImages';
+import reportWebVitals from "./reportWebVitals";
+import Provider from "../src/context/FirestoreContext";
+import AuthProvider, { useAuthContext } from "./context/AuthContext";
+import StockImages from "./components/StockImages";
 import Single from "./components/Single";
+import NotFound from "./components/NotFound";
+import Profile from "./components/profile";
+function AppRoutes() {
+  const { currentUser } = useAuthContext();
 
-function AppRoutes(){
-const {currentUser} = useAuthContext();
-
-  return(
-   <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/images/:id' element={<Single/>} />
-      {currentUser && 
-      <Route path='/StockImages' element={<StockImages/>}/>}
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/images/:id" element={<Single />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/profile" element={<Profile />} />
+      {currentUser && <Route path="/StockImages" element={<StockImages />} />}
     </Routes>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <AuthProvider>
-    <Provider>
-      <Router>
-        <Layout>
-          <AppRoutes/>
-        </Layout>
-      </Router>
-    </Provider>
-  </AuthProvider>
+    <AuthProvider>
+      <Provider>
+        <Router>
+          <Layout>
+            <AppRoutes />
+          </Layout>
+        </Router>
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>
 );
-
-        
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
